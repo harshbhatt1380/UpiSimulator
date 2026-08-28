d118595b8266543b74d8fc6d09dbe5c10c62c6be
harshbhatt1380 <janmijaybhatt222@gmail.com>
2026-08-28 22:48:00 +0530

    Progress made in Authentication and now the protected endpoints are reachable via token, the things I learnt was use of Authentication class and SecurityContextHolder however I do not understand them fully or well enough I believe

--- src/main/java/com/backendproject/upisimulator/security/JwtAuthFilter.java
+++ src/main/java/com/backendproject/upisimulator/security/JwtAuthFilter.java
@@ -1,14 +1,22 @@
 package com.backendproject.upisimulator.security;
 
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.backendproject.upisimulator.service.JwtService;
 import com.backendproject.upisimulator.service.MyUserDetailService;
 
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class JwtAuthFilter extends OncePerRequestFilter
 {
     private final JwtService jwtService;
@@ -18,14 +26,17 @@ public class JwtAuthFilter extends OncePerRequestFilter
         this.jwtService=jwtService;
         this.myUserDetailService=myUserDetailService;
     } 
-    protected void doFilterInternal(HttpServletRequest request,HttpServletResponse response,FilterChain filterChain)
+    protected void doFilterInternal(HttpServletRequest request,HttpServletResponse response,FilterChain filterChain)throws IOException,ServletException
     {
         String authHeader = request.getHeader("Authorization");
-        if(authHeader!=null && authHeader.startsWith("Bearer"))
+        if(authHeader!=null && authHeader.startsWith("Bearer "))
         {
             String token = authHeader.substring(7);
             String email = jwtService.validateToken(token);
-            myUserDetailService.fetchUser(email);
+            MyUserDetail userDetail = myUserDetailService.fetchUser(email);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(userDetail, null, userDetail.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication); 
         }
+        filterChain.doFilter(request, response);
     }
 }
--- src/main/java/com/backendproject/upisimulator/security/SecurityConfig.java
+++ src/main/java/com/backendproject/upisimulator/security/SecurityConfig.java
@@ -6,10 +6,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig 
 {
+    private final JwtAuthFilter jwtAuthFilter;
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter)
+    {
+        this.jwtAuthFilter=jwtAuthFilter;
+    }
     @Bean
     public PasswordEncoder passwordEncoder()
     {
@@ -19,7 +25,7 @@ public class SecurityConfig
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
     {
-        http.csrf(csrf->csrf.disable()).authorizeHttpRequests(auth->auth.requestMatchers("/users/register","/users/login").permitAll().anyRequest().authenticated());
+        http.csrf(csrf->csrf.disable()).authorizeHttpRequests(auth->auth.requestMatchers("/users/register","/users/login").permitAll().anyRequest().authenticated()).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
